1db55ef382c27ca3e595a217a808be8b8f32c0e0
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-12-10 21:42:33 -0800

    Add tracing-subscriber to dep

--- src/main.rs
+++ src/main.rs
@@ -35,12 +35,13 @@ async fn main() {
             .expect("failed to build tokio runtime")
             .block_on(entrypoint());
     } else {
-        use cdn_server::entrypoint;
+        // use cdn_server::entrypoint;
 
-        tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .expect("failed to build tokio runtime")
-            .block_on(entrypoint());
+        // tokio::runtime::Builder::new_multi_thread()
+        //     .enable_all()
+        //     .build()
+        //     .expect("failed to build tokio runtime")
+        //     .block_on(entrypoint());
+        println!("Server mode not yet implemented");
     }
 }
