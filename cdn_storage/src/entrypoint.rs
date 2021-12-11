b5f091d2a600b3dbf9806407567328731470ee9d
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-12-11 11:46:25 -0800

    Match its kind

--- cdn_storage/src/entrypoint.rs
+++ cdn_storage/src/entrypoint.rs
@@ -12,7 +12,7 @@ use std::fs;
 
 pub async fn entrypoint() {
     fs::create_dir_all(STORAGE_PATH.to_string())
-        .map_err(|e| match e {
+        .map_err(|e| match e.kind() {
             AlreadyExists => (),
             _ => panic!("Failed to create uploads directory: {:?}", e)
         });
